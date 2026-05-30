863ee1be690e01e227c68b5c678cc631ebda4133
TsukuyomiT23 <moisessantosa@gmail.com>
2026-05-30 19:50:41 +0100

    sending the asteroid feature add to game

--- SoSimples/main.py
+++ SoSimples/main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import *
 from player import Player
 from logger import log_state
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
 
 def main():
     pygame.init()
@@ -11,28 +13,44 @@ def main():
     )
     clock = pygame.time.Clock()
     dt = 0
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
     player = Player(
         SCREEN_WIDTH / 2,
         SCREEN_HEIGHT / 2
     )
+
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroidField = AsteroidField()
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         log_state()
+
         for event in pygame.event.get():
             pass
+
         screen.fill("black")
+
         updatable.update(dt)
+
         for obj in drawable:
             obj.draw(screen)
+
         pygame.display.flip()
+
         dt = clock.tick(60) / 1000
 
 
