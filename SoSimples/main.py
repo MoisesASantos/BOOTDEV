75d308c882dbae20c98042531d39ece2f16d6f25
TsukuyomiT23 <moisessantosa@gmail.com>
2026-05-30 18:33:44 +0100

    add the move left and right features

--- SoSimples/main.py
+++ SoSimples/main.py
@@ -1,10 +1,14 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import *
 
 def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	clock = pygame.time.Clock()
+	dt = 0.0
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	print("Starting Asteroids")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
@@ -17,7 +21,10 @@ def main():
 			pass
 		#this method I use to set a screen on black
 		screen.fill("black")
+		player.update(dt)
+		player.draw(screen)
 		#I used this method for refresh the window
+		dt = clock.tick(60) / 1000
 	display.flip()
 
 if __name__ == "__main__":
